d1743156240e6367e7ec274a9e32a364031b10e2
manoj <manojshakya1207@gmail.com>
2020-11-25 12:58:06 +0530

    fgg

--- app/src/main/java/com/uav/autodebit/Activity/CreditCardBill.java
+++ app/src/main/java/com/uav/autodebit/Activity/CreditCardBill.java
@@ -1,18 +1,38 @@
 package com.uav.autodebit.Activity;
 
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import androidx.cardview.widget.CardView;
+import androidx.core.content.res.ResourcesCompat;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.squareup.picasso.Picasso;
 import com.uav.autodebit.Interface.VolleyResponse;
 import com.uav.autodebit.R;
+import com.uav.autodebit.exceptions.ExceptionsNotification;
+import com.uav.autodebit.util.Utility;
+import com.uav.autodebit.vo.BeneAccVO;
+
+import java.util.ArrayList;
 
 public class CreditCardBill extends Base_Activity implements View.OnClickListener {
 
     ImageView back_activity_button;
+    LinearLayout addcardlistlayout,layoutmainBanner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +41,63 @@ public class CreditCardBill extends Base_Activity implements View.OnClickListene
         if(getSupportActionBar()!=null)  getSupportActionBar().hide();
 
         back_activity_button=findViewById(R.id.back_activity_button);
+        addcardlistlayout=findViewById(R.id.addcardlistlayout);
+        layoutmainBanner=findViewById(R.id.layoutmainBanner);
         back_activity_button.setOnClickListener(this);
 
 
         getCreditCardList(CreditCardBill.this,new VolleyResponse((VolleyResponse.OnSuccess)(success)->{
 
-            startActivity(new Intent(CreditCardBill.this,Credit_Card_Webview.class));
 
+            try {
+
+
+                ArrayList<BeneAccVO> beneAccVOS= (ArrayList<BeneAccVO>) new Gson().fromJson("", new TypeToken<ArrayList<BeneAccVO>>() { }.getType());
+                if(beneAccVOS!=null && beneAccVOS.size()>0){
+                   /* showAddCardBtn();
+                    getdata(beneAccVOS);*/
+                }else{
+                    Intent  intent = new Intent(CreditCardBill.this,Credit_Card_Webview.class);
+                    intent.putExtra(Credit_Card_Webview.EXTRAS_TITLE,"Credit Card Bill Pay");
+                    intent.putExtra(Credit_Card_Webview.EXTRAS_URL,"https://www.google.com/");
+                    intent.putExtra(Credit_Card_Webview.EXTRAS_FAIL_URL,"fail");
+                    intent.putExtra(Credit_Card_Webview.EXTRAS_SUCCESS_URL,"success");
+                    intent.putExtra(Credit_Card_Webview.EXTRAS_TXN_ID,"txnid");
+                    startActivity(new Intent(intent));
+                    finish();
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+                ExceptionsNotification.ExceptionHandling(CreditCardBill.this, Utility.getStackTrace(e));
+            }
         }));
+    }
 
+    @SuppressLint("ResourceType")
+    public void showAddCardBtn(){
+        TextView textView = Utility.getTextView(CreditCardBill.this,"Add Credit Card");
+        textView.setPaintFlags(textView.getPaintFlags() |   Paint.UNDERLINE_TEXT_FLAG);
+        textView.setTextColor(getApplication().getResources().getColorStateList(R.drawable.text_change_color_blue_color_black));
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 17);
+        textView.setTag("addBeneficiaryBtn");
+
+        Typeface typeface = ResourcesCompat.getFont(this, R.font.poppinssemibold);
+        textView.setTypeface(typeface ,Typeface.BOLD);
+
+        int childCount = layoutmainBanner.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View v = layoutmainBanner.getChildAt(i);
+            if((v.getTag()!=null) && (v.getTag().equals("addBeneficiaryBtn"))){
+                Utility.removeEle(v);
+            }
+        }
+        layoutmainBanner.addView(textView);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Utility.removeEle(textView);
+            }
+        });
     }
 
     private void getCreditCardList(Context context , VolleyResponse volleyResponse) {
--- app/src/main/java/com/uav/autodebit/Activity/Credit_Card_Webview.java
+++ app/src/main/java/com/uav/autodebit/Activity/Credit_Card_Webview.java
@@ -2,21 +2,42 @@ package com.uav.autodebit.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.ConsoleMessage;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.uav.autodebit.R;
+import com.uav.autodebit.exceptions.ExceptionsNotification;
 import com.uav.autodebit.override.UAVProgressDialog;
 import com.uav.autodebit.util.DialogInterface;
 import com.uav.autodebit.util.Utility;
 
-public class Credit_Card_Webview extends AppCompatActivity implements View.OnClickListener {
+public class Credit_Card_Webview extends AppCompatActivity implements View.OnClickListener , MyJavaScriptInterface.javascriptinterface{
     public static final String EXTRAS_TITLE = "title";
     public static final String EXTRAS_URL = "url";
     public static final String EXTRAS_FAIL_URL = "fail_url";
@@ -48,6 +69,262 @@ public class Credit_Card_Webview extends AppCompatActivity implements View.OnCli
         txn_id=getIntent().getStringExtra(EXTRAS_TXN_ID);
         fail_url=getIntent().getStringExtra(EXTRAS_FAIL_URL)+ "app/";
         success_url=getIntent().getStringExtra(EXTRAS_SUCCESS_URL)+ "app/";
+
+        openWebView(url);
+    }
+
+
+    @SuppressLint("SetJavaScriptEnabled")
+    void openWebView(final String receiptUrl) {
+        Log.w("callUrl", receiptUrl);
+        webView=findViewById(R.id.webView);
+        webView.setVerticalScrollBarEnabled(false);
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.setWebViewClient(new Credit_Card_Webview.MyBrowser());
+        webView.getSettings().setBuiltInZoomControls(false);
+        webView.setInitialScale(1);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setSupportMultipleWindows(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setMinimumFontSize(16);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            WebView.enableSlowWholeDocumentDraw();
+        }
+        settings.setDomStorageEnabled(true);
+
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                if (!Credit_Card_Webview.this.isFinishing() && progressBar != null && !progressBar.isShowing()) {
+                    try {
+                        progressBar.show();
+                    } catch (Exception e) {
+
+                    }
+                }
+                if (newProgress == 100) {
+                    Utility.dismissDialog(Credit_Card_Webview.this, progressBar);
+                }
+            }
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                return super.onJsAlert(view, url, message, result);
+            }
+            @Override
+            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+                android.util.Log.d("WebView", consoleMessage.message());
+                return true;
+            }
+            @Override
+            public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture, Message resultMsg) {
+                newWebView = new WebView(Credit_Card_Webview.this);
+                newWebView.getSettings().setJavaScriptEnabled(true);
+                newWebView.getSettings().setBuiltInZoomControls(true);
+                newWebView.setInitialScale(1);
+                newWebView.getSettings().setUseWideViewPort(true);
+                newWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+                //for download PDF files
+
+                newWebView.setWebChromeClient(new WebChromeClient() {
+
+                    @Override
+                    public void onCloseWindow(WebView window) {
+                        super.onCloseWindow(window);
+                        if (newWebView != null) {
+                            try {
+                                webView.removeView(newWebView);
+                            } catch (Exception e) {
+                            }
+                        }
+                    }
+
+                    public void onProgressChanged(WebView view, int newProgress) {
+                        super.onProgressChanged(view, newProgress);
+                        if (!Credit_Card_Webview.this.isFinishing() && progressBar != null && !progressBar.isShowing()) {
+                            try {
+                                progressBar.show();
+                            } catch (Exception e) {
+                            }
+                        }
+                        if (newProgress == 100) {
+                            Utility.dismissDialog(Credit_Card_Webview.this, progressBar);
+                        }
+                    }
+                });
+                newWebView.setWebViewClient(new WebViewClient());
+                LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+                params.gravity = Gravity.CENTER;
+                newWebView.setLayoutParams(params);
+
+                webView.addView(newWebView);
+                WebView.WebViewTransport transport = (WebView.WebViewTransport) resultMsg.obj;
+                transport.setWebView(newWebView);
+                resultMsg.sendToTarget();
+
+                newWebView.setWebViewClient(new WebViewClient() {
+                    @Override
+                    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                        try {
+                            if (url.contains(".pdf")) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW);
+                                intent.setDataAndType(Uri.parse(url), "application/pdf");
+                                try {
+                                    if (newWebView != null) {
+                                        try {
+                                            webView.removeView(newWebView);
+                                        } catch (Exception e) {
+                                        }
+                                    }
+                                    view.getContext().startActivity(intent);
+                                } catch (ActivityNotFoundException e) {
+                                    //user does not have a pdf viewer installed
+                                }
+                            } else {
+                                view.loadUrl(url);
+                            }
+                            return true;
+                        } catch (Exception e) {
+                            return true;
+                        }
+                    }
+                    @Override
+                    public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                        super.onPageStarted(view, url, favicon);
+                        Log.w("pagestart", url);
+                    }
+                    @Override
+                    public void onPageFinished(WebView view, String url) {
+                        Log.w("newWebcie", url);
+                        try {
+                            if (webView != null) {
+                                webView.scrollTo(0, 0);
+                            }
+                        } catch (Exception e) {
+                            ExceptionsNotification.ExceptionHandling(Credit_Card_Webview.this, Utility.getStackTrace(e), "0");
+                        }
+
+                    }
+
+                    @SuppressWarnings("deprecation")
+                    public void onReceivedError(WebView view, int errorCode,
+                                                String description, String failingUrl) {
+                        showError(description, progressBar);
+                    }
+
+                    @TargetApi(android.os.Build.VERSION_CODES.M)
+                    public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                        showError((String) error.getDescription(), progressBar);
+                    }
+
+                    @TargetApi(android.os.Build.VERSION_CODES.M)
+                    public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
+                        showError(errorResponse.getReasonPhrase().toString(), progressBar);
+                    }
+                });
+                return true;
+            }
+
+            @Override
+            public void onCloseWindow(WebView window) {
+                super.onCloseWindow(window);
+                try {
+                    newWebView.destroy();
+                } catch (Exception e) {
+                    ExceptionsNotification.ExceptionHandling(Credit_Card_Webview.this, Utility.getStackTrace(e), "0");
+                }
+                Utility.dismissDialog(Credit_Card_Webview.this, progressBar);
+            }
+        });
+        
+        webView.setWebViewClient(new MyBrowser());
+        webView.addJavascriptInterface(new MyJavaScriptInterface(this), "HTMLOUT");
+        webView.loadUrl(receiptUrl); //receiptUrl
+
+    }
+
+    @Override
+    public void htmlresult(String result) {
+        Toast.makeText(this, ""+result, Toast.LENGTH_SHORT).show();
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Log.w("URL", url);
+
+            if (url.contains(".pdf")) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setDataAndType(Uri.parse(url), "application/pdf");
+                try {
+                    if (newWebView != null) {
+                        try {
+                            webView.removeView(newWebView);
+                        } catch (Exception e) {
+                        }
+                    }
+                    view.getContext().startActivity(intent);
+                } catch (ActivityNotFoundException e) {
+                    //user does not have a pdf viewer installed
+                }
+            }else if(url.contains("upi:")){
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity( intent );
+            }else{
+                view.loadUrl(url);
+            }
+            return true;
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            Utility.dismissDialog(Credit_Card_Webview.this, progressBar);
+            try {
+                if (url.equalsIgnoreCase(success_url) || url.equalsIgnoreCase(fail_url)) {
+                    webView.setVisibility(View.GONE);
+                    webView.loadUrl("javascript:HTMLOUT.showHTML(document.getElementById('resp').innerHTML);");
+                    webView.loadUrl("javascript:console.log('MAGIC'+document.getElementById('resp').innerHTML);");
+                }
+            } catch (Exception e) {
+                ExceptionsNotification.ExceptionHandling(Credit_Card_Webview.this, Utility.getStackTrace(e));
+            }
+        }
+
+        @SuppressWarnings("deprecation")
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            showError(description, progressBar);
+        }
+
+        @TargetApi(android.os.Build.VERSION_CODES.M)
+        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+            showError((String) error.getDescription(), progressBar);
+        }
+
+        @TargetApi(android.os.Build.VERSION_CODES.M)
+        public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
+            showError(errorResponse.getReasonPhrase().toString(), progressBar);
+        }
+    }
+
+    private void showError(String description, Dialog progressBar) {
+        Utility.dismissDialog(Credit_Card_Webview.this, progressBar);
+        try {
+            if (webView != null) {
+                webView.scrollTo(0, 0);
+            }
+        } catch (Exception e) {
+            ExceptionsNotification.ExceptionHandling(Credit_Card_Webview.this, Utility.getStackTrace(e), "0");
+        }
+        Log.e("weverrir", description);
     }
 
     @Override
